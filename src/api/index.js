a7803dd4eea4667d75e84414609a4e65a195312f
flexmodule <1181316781@qq.com>
2018-12-10 23:13:15 +0800

    新增

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import request from '@/utils/request.js';
-var funcArr=["auth","getindexdata","getroutedata","searchInfo","getFoodDetail","getshoplist","collectFood","uncollectFood","getroutedetail","getorderlist"];
+var funcArr=["auth","getindexdata","getperson","getroutedata","searchInfo","getFoodDetail","getshoplist","collectFood","uncollectFood","getroutedetail","getorderlist","getorderdetail","getcollectlist"];
 var apifunc={}
 funcArr.forEach(function(item,index){
   apifunc[item] =function item(url, oType, data) {
